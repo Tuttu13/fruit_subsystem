463ce89a8c4bc2c73374fb3effa82a7bae8754d4
Tuttu_13 <willson30426@yahoo.co.jp>
2024-01-25 20:38:33 +0900

    setting.py 設定

--- fruits_manager/fruits_manager/settings.py
+++ fruits_manager/fruits_manager/settings.py
@@ -41,6 +41,8 @@ INSTALLED_APPS = [
     'fruits_manager',
 ]
 
+AUTH_USER_MODEL = "accounts.User" 
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
