926d7622f5a3ccba6e6f76982089e8d027238e90
Tuttu_13 <willson30426@yahoo.co.jp>
2024-01-28 06:55:13 +0900

    ログイン画面　関連

--- fruit_sales_manager/account/views.py
+++ fruit_sales_manager/account/views.py
@@ -1,16 +1,35 @@
-from django.shortcuts import render
+#_app/views.py
+from django.db import IntegrityError
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.models import User
 
-def signup_view(request):
-    pass
+TARGET_DIR = 'account/'
 
-def login_view(request):
-    pass
+def signupfunc(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
 
-def logout_view(request):
-    pass
+        try:
+            user = User.objects.create_user(username, '', password)
+            return render(request, 'account/login.html', {})
+        except IntegrityError:
+            return render(request, 'account/signup.html', {'error': 'このユーザはすでに登録されています'})
 
-def user_view(request):
-    pass
+    return render(request, TARGET_DIR+'signup.html', {})
 
-def other_view(request):
-    pass
+def loginfunc(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            return redirect('index')
+        
+        else:
+            return render(request, 'account/login.html', {})
+    
+    return render(request, TARGET_DIR+'login.html', {})
