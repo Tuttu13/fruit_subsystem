48431f6083c716a6b8578693f372c3880c3ccbea
Tuttu_13 <willson30426@yahoo.co.jp>
2024-01-27 17:56:54 +0900

    test

--- fruits_manager/fruits_manager/settings.py
+++ fruits_manager/fruits_manager/settings.py
@@ -11,9 +11,10 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+TEMPLATE_DIR = os.path.join(BASE_DIR,'templates')
 
 
 # Quick-start development settings - unsuitable for production
@@ -38,10 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'account',
-    'fruits_manager',
+    'fruit_app',
 ]
-
-AUTH_USER_MODEL = "accounts.User" 
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -58,7 +57,7 @@ ROOT_URLCONF = 'fruits_manager.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'templates'], 
+        'DIRS': [os.path.join(BASE_DIR, 'fruits_manager/templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
