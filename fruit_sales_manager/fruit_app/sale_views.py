ede848793f07e0a8e728a06e9c056a3a0c4eb967
Tuttu_13 <tutui0426@yahoo.co.jp>
2024-02-01 05:38:07 +0900

    売り上げ情報画面関連　修正

--- fruit_sales_manager/fruit_app/sale_views.py
+++ fruit_sales_manager/fruit_app/sale_views.py
@@ -1,15 +1,15 @@
-from .forms import CSVUploadForm
-from .models import FruitsSalesInfo
 
-# クラスベース
+from .models import FruitsSalesInfo, FruitsMaster
+
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.views.generic import ListView,UpdateView,DeleteView,CreateView
+from django.views.generic import ListView,DeleteView
 from django.views.generic.list import ListView
-from django.views import generic
+from django.shortcuts import render
 from django.urls import reverse_lazy, reverse
-from fruit_sales_manager import cmn_func as cf
+from django.shortcuts import redirect
+from django.contrib.auth.decorators import login_required
 
-class SaleListView(LoginRequiredMixin, ListView):
+class SaleListView(LoginRequiredMixin,ListView):
     template_name = 'fruit_sale/sale_list.html'
     model = FruitsSalesInfo
     
@@ -19,29 +19,62 @@ class SaleListView(LoginRequiredMixin, ListView):
 
         return queryset
 
-class SaleCreateView(CreateView):
-    pass
-
-class SaleUpdateView(LoginRequiredMixin,UpdateView):
-    template_name = "fruit_sale/sale_edit.html"
-    model = FruitsSalesInfo
-    # form_class = FruitForm
-
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context = cf.fix_data()
-        return context
-
-    def get_success_url(self):
-        return reverse("list")
-
 class SaleDeleteView(LoginRequiredMixin, DeleteView):
     template_name = 'fruit_sale/csvimport.html'
     model = FruitsSalesInfo
     success_url = reverse_lazy('sales')
 
-class CsvImportView(generic.FormView):
-    template_name = 'fruit_sale/sale_list.html'
-    form_class = CSVUploadForm
-    success_url = reverse_lazy('sales')
+@login_required
+def createsale(request):
+
+    template_name = 'fruit_sale/sale_add.html'
+    fruits = FruitsMaster.objects.values('fruit_name')
+    context = {'fluits_list': fruits,}
+
+    if request.method == 'POST':
+        data = request.POST
+        target_fruit = data['fruit_name']
+        sales = data['sales']
+        total = _get_total(target_fruit, sales)
+        sales_at = data['saledate']
+
+        FruitsSalesInfo.objects.create(
+            fruit_name=target_fruit,
+            sales=sales,
+            total=total,
+            sales_at=sales_at
+        )
+        return redirect(reverse('sales')) 
     
+    return render(request, template_name, context)\
+
+@login_required
+def editsale(request, pk):
+
+    template_name = 'fruit_sale/sale_edit.html'
+    fruits = FruitsMaster.objects.values('fruit_name')
+    context = {'fluits_list': fruits,}
+
+    if request.method == 'POST':
+        data = request.POST
+        target_fruit = data['fruit_name']
+        sales = data['sales']
+        total = _get_total(target_fruit, sales)
+        sales_at = data['saledate']
+
+        FruitsSalesInfo.objects.filter(pk=pk).update(
+            fruit_name=target_fruit,
+            sales=sales,
+            total=total,
+            sales_at=sales_at
+        )
+        return redirect(reverse('sales')) 
+    
+    return render(request, template_name, context)
+
+def _get_total(target_fruit, sales):
+# .get(target_fruit)
+    test = FruitsMaster.objects.values_list('price').get(fruit_name='{0}'.format(target_fruit))
+    total = test[0] * int(sales)
+
+    return total
