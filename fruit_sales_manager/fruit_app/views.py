7d58d20e0b3b95588819e06f7f367dedb22676d1
Tuttu_13 <tutui0426@yahoo.co.jp>
2024-02-10 09:24:28 +0900

    importの並び替え

--- fruit_sales_manager/fruit_app/views.py
+++ fruit_sales_manager/fruit_app/views.py
@@ -1,10 +1,4 @@
 from django.views.generic import TemplateView
-
-from django.contrib.auth.decorators import login_required
-from django.shortcuts import redirect
-from django.urls import reverse
-
-
 class IndexView(TemplateView):
     template_name = 'index.html' 
 
