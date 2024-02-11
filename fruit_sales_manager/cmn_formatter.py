1a01b49f59f593c83935c6152d0f51082c1a9191
Tuttu_13 <tutui0426@yahoo.co.jp>
2024-02-11 20:25:36 +0900

    カタカナ変換の正規表現修正

--- fruit_sales_manager/cmn_formatter.py
+++ fruit_sales_manager/cmn_formatter.py
@@ -8,7 +8,7 @@ class Cmn_Fomatter():
 
     def check_kata_format(target_fruit):
 
-        re_hiragana = re.compile(r'^[あ-ん]+$')
+        re_hiragana = re.compile(r'^[あ-んー]+$')
         if re_hiragana.fullmatch(target_fruit):
             kata_fruit_name = jaconv.hira2kata(target_fruit)
             return kata_fruit_name
