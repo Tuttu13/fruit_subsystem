5f08243dc11cdc94e9013563868315b6904b4390
Tuttu_13 <tutui0426@yahoo.co.jp>
2024-02-11 17:01:13 +0900

    果物名が正しい値か確認するための処理を共通化

--- fruit_sales_manager/cmn_validation.py
+++ fruit_sales_manager/cmn_validation.py
@@ -1,12 +1,70 @@
 from datetime import datetime
+
+import jaconv
+
+kata_fruits_list = [
+    'アケビ',
+    'アセロラ',
+    'アボカド',
+    'アンズ',
+    'イチゴ',
+    'イチジク',
+    'ウメ',
+    'カキ',
+    'キウイフルーツ',
+    'クリ',
+    'グレープフルーツ',
+    'ココヤシ',
+    'サクランボ',
+    'ザクロ',
+    'スイカ',
+    'スターフルーツ',
+    'スモモ',
+    'ドリアン',
+    'パイナップル',
+    'ナシ',
+    'バナナ',
+    'パパイヤ',
+    'ブドウ',
+    'ブルーベリー',
+    'マンゴー',
+    'ミカン',
+    'ミラクルフルーツ',
+    'メロン',
+    'モモ',
+    'ライチ',
+    'ラフランス',
+    'リンゴ',
+    'レモン',
+]
 class Cmn_Validation():
     
     def check_object_format(row):
 
         try:
-            isinstance(row[0], str)
-            isinstance(row[1], int)
-            isinstance(row[2], int)
-            isinstance(datetime.strptime(row[3], '%Y-%m-%d %H:%M'), datetime)
+            fruit_name = row[0]
+            Cmn_Validation.check_fruit_name(fruit_name)
+            sales = row[1]
+            isinstance(sales, int)
+            total = row[2]
+            isinstance(total, int)
+            str_time = row[3]
+            isinstance(datetime.strptime(str_time, '%Y-%m-%d %H:%M'), datetime)
         except:
+            raise
+
+    def check_fruit_name(fruit_name):
+
+        isinstance(fruit_name, str)
+
+        hira_fruits_list = []
+        for fruit in kata_fruits_list:
+            hira_fruit = jaconv.kata2hira(fruit)
+            hira_fruits_list.append(hira_fruit)
+
+        if fruit_name in kata_fruits_list:
+            pass
+        elif fruit_name in hira_fruits_list:
+            pass
+        else:
             raise
