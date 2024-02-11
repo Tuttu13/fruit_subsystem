73ad0a8acab6ebbe0edce5b39df1f69e23acf891
Tuttu_13 <tutui0426@yahoo.co.jp>
2024-02-11 16:55:37 +0900

    新規フルーツ追加するAPIを関数ベースに変更

--- fruit_app/urls.py
+++ fruit_app/urls.py
@@ -7,7 +7,8 @@ urlpatterns = [
     path('', views.IndexView.as_view(), name='index'),
     # 果物マスタ
     path('list', fruit_views.FruitListView.as_view(), name='list'),
-    path('create', fruit_views.FruitCreateView.as_view(), name='create'),
+    # path('create', fruit_views.FruitCreateView.as_view(), name='create'),
+    path('create', fruit_views.createfruit, name='create'),
     path('update/<int:pk>', fruit_views.editsale, name='update'),
     path('delete/<int:pk>', fruit_views.FruitDeleteView.as_view(), name='delete'),
 ]
