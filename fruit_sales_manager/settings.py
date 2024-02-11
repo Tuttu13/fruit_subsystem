21a181d6bb59a6e440c4fc798aa647d7ba4b2e68
Tuttu_13 <tutui0426@yahoo.co.jp>
2024-02-11 11:44:20 +0900

    setting修正

--- fruit_sales_manager/settings.py
+++ fruit_sales_manager/settings.py
@@ -110,11 +110,7 @@ AUTH_PASSWORD_VALIDATORS = [
 LANGUAGE_CODE = 'ja'
 
 TIME_ZONE = 'Asia/Tokyo'
-
-USE_I18N = True
-
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
